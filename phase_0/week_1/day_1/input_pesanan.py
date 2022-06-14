34b9673d2d05989e85aee2ff9b45f0c53e6b078b
Muhamad Ivan <mivanmuhamadivan@gmail.com>
2022-06-14 18:54:10 +0700

    Materi 14 Juni 2022

---
+++ phase_0/week_1/day_1/input_pesanan.py
@@ -0,0 +1,8 @@
+pesanan = []
+pesan = input("Masukkan pesanan pertama : ")
+pesanan.append(pesan)
+pesan = input("Masukkan pesanan kedua : ")
+pesanan.append(pesan)
+pesan = input("Masukkan pesanan ketiga : ")
+pesanan.append(pesan)
+print(f"pesanan anda : {pesanan}")
